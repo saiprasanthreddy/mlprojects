5dcccc99f79fd9e07c4594d9756fa378e482e0b8
unknown <saiprasanthq170@gmail.com>
2026-08-08 06:41:04 +0530

    model tranning

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import sys
 import dill
 import os
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
@@ -15,3 +16,25 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+
+            y_test_pred = model.predict(X_test)
+            y_train_pred = model.predict(X_train)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            report[list(models.keys())[i]] = {"test_score": test_model_score, "train_score": train_model_score}
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)    
